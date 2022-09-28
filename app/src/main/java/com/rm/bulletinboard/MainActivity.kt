91891013cea621ae3ffed6f632b4096b808f200d
alulcardino <52010205+alulcardino@users.noreply.github.com>
2022-09-29 02:18:57 +0600

    viewbinding is added

--- app/src/main/java/com/rm/bulletinboard/MainActivity.kt
+++ app/src/main/java/com/rm/bulletinboard/MainActivity.kt
@@ -8,21 +8,24 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.main_content.*
+import com.rm.bulletinboard.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
+    private lateinit var rootElement : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        rootElement = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(rootElement.root)
         init();
     }
 
     private fun init() {
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
+        val toggle = ActionBarDrawerToggle(this, rootElement.drawerLayout, rootElement.mainContent.toolbar, R.string.open, R.string.close)
+        rootElement.drawerLayout.addDrawerListener(toggle)
         toggle.syncState();
-        navView.setNavigationItemSelectedListener (this)
+        rootElement.navView.setNavigationItemSelectedListener (this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -59,7 +62,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
             }
         }
-        return true;
+         return true;
     }
 
 }
